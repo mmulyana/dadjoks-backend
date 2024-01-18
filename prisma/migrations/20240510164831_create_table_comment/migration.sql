80aeea858d079fdf40981148b8e0f5939119c99a
mmulyana <mulyan.t20@gmail.com>
2024-01-18 13:00:00 +0700

    feat: table comment

---
+++ prisma/migrations/20240510164831_create_table_comment/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `Comment` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `text` VARCHAR(191) NOT NULL,
+    `authorId` INTEGER NOT NULL,
+    `createdAt` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updateddAt` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
